cba78be96284171f0ad6a319aafaf39f3a603472
wilsonleyva2023 <wilsonleyva2023@gmail.com>
2026-07-19 01:57:48 -0300

    Update recibos.js

--- asistente-trabajo/src/services/recibos.js
+++ asistente-trabajo/src/services/recibos.js
@@ -1,13 +1,20 @@
 const { supabase } = require('../db');
 
-async function crearRecibo({ cliente_id, presupuesto_id, concepto, monto }) {
+async function crearRecibo({ cliente_id, presupuesto_id, concepto, monto, es_pago_parcial }) {
   const { data, error } = await supabase
     .from('recibos')
-    .insert([{ cliente_id, presupuesto_id: presupuesto_id || null, concepto, monto }])
+    .insert([{ cliente_id, presupuesto_id: presupuesto_id || null, concepto, monto, es_pago_parcial: !!es_pago_parcial, es_pago_completo: !es_pago_parcial }])
     .select()
     .single();
   if (error) throw error;
   return data;
+}
+
+async function contarHoy() {
+  const inicio = new Date(); inicio.setHours(0, 0, 0, 0);
+  const { data, error } = await supabase.from('recibos').select('id').gte('creado_en', inicio.toISOString());
+  if (error) throw error;
+  return (data || []).length;
 }
 
 async function recibosPorCliente(cliente_id) {
@@ -27,4 +34,4 @@ async function recibosEnRango(desdeISO, hastaISO) {
   return data;
 }
 
-module.exports = { crearRecibo, recibosPorCliente, recibosEnRango };
+module.exports = { crearRecibo, recibosPorCliente, recibosEnRango, contarHoy };
